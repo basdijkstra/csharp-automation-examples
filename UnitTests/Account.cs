13785fba6552166663c21126b18458deaf3315f6
Bas Dijkstra <bas@ontestautomation.com>
2020-08-26 17:32:42 +0200

    Initial commit with unit testing examples

---
+++ UnitTests/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests
+{
+    class Account
+    {
+        public AccountType AccountType { get; set; }
+        public int Balance { get; set; }
+
+        public Account(AccountType accountType, int balance)
+        {
+            AccountType = accountType;
+            Balance = balance;
+        }
+
+        public void Withdraw(int amount)
+        {
+            if (amount > Balance && AccountType == AccountType.SAVINGS)
+            {
+                throw new Exception($"Insufficient balance: cannot withdraw {amount} when balance is {Balance}");
+            }
+            else
+            {
+                Balance -= amount;
+            }
+        }
+    }
+}
---
+++ UnitTests/AccountTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class AccountTest
+    {
+        [Test]
+        public void SavingsAccount_SufficientFunds_ExpectSuccessfulWithdrawal()
+        {
+            Account account = new Account(AccountType.SAVINGS, 1000);
+
+            account.Withdraw(999);
+
+            Assert.That(account.Balance, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void CheckingAccount_InsufficientFunds_ExpectSuccessfulWithdrawal()
+        {
+            Account account = new Account(AccountType.CHECKING, 1000);
+
+            account.Withdraw(1005);
+
+            Assert.That(account.Balance, Is.EqualTo(-5));
+        }
+
+        [Test]
+        public void SavingsAccount_InsufficientFunds_ExpectSuccessfulWithdrawal()
+        {
+            Account account = new Account(AccountType.SAVINGS, 1000);
+
+            account.Withdraw(1005);
+
+            Assert.That(account.Balance, Is.EqualTo(-5));
+        }
+    }
+}
