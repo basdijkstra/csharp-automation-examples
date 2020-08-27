31cea2493d32960232294259f53a4bce1414919c
Bas Dijkstra <bas@ontestautomation.com>
2020-08-27 09:34:43 +0200

    Added examples for API tests

---
+++ ApiTests/AccountTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using RestSharp;
+
+namespace ApiTests
+{
+    [TestFixture]
+    public class AccountTests
+    {
+
+        [Test]
+        public void RetrieveDataForAccount12345_CheckStatusCode_ShouldBeHttp200()
+        {
+            // arrange
+            RestClient client = new RestClient("http://parabank.parasoft.com/parabank/services/bank");
+            RestRequest request = new RestRequest("/accounts/12345", Method.GET);
+
+            // act
+            IRestResponse response = client.Execute(request);
+
+            // assert
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Test]
+        public void RetrieveDataForAccount99999_CheckStatusCode_ShouldBeHttp404()
+        {
+            // arrange
+            RestClient client = new RestClient("http://parabank.parasoft.com/parabank/services/bank");
+            RestRequest request = new RestRequest("/accounts/10101", Method.GET);
+
+            // act
+            IRestResponse response = client.Execute(request);
+
+            // assert
+            Assert.AreEqual(System.Net.HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+}
